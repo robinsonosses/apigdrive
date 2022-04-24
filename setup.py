af57acfe60b4452ed8291dfaab3c136815c564b7
robinsonosses <robinsonosses@gmail.com>
2022-04-24 19:27:08 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ install_requires = \
 
 setup_kwargs = {
     'name': 'fastapi-cloud-drives',
-    'version': '0.1.5',
+    'version': '0.1.6',
     'description': 'Module for FastAPI to work with cloud drives',
     'long_description': '<h3 align="center">FastAPI Cloud Drives</h3>\n\n<div align="center">\n\n[![Status](https://img.shields.io/badge/status-active-success.svg)]()\n[![GitHub Issues](https://img.shields.io/github/issues/kylelobo/The-Documentation-Compendium.svg)](https://github.com/MadeByMads/fastapi-cloud-drives/issues)\n[![GitHub Pull Requests](https://img.shields.io/github/issues-pr/kylelobo/The-Documentation-Compendium.svg)](https://github.com/MadeByMads/fastapi-cloud-drives/pulls)\n[![License](https://img.shields.io/badge/license-MIT-blue.svg)](/LICENSE)\n\n</div>\n\n\n## 🧐 About <a name = "about"></a>\n\nThe FastAPI Cloud Drives module supports Google Drive, OneDrive, Dropbox cloud storage providers. You can easily search, upload, download files from this cloud providers. \n\n\n## 🏁 Getting Started <a name = "getting_started"></a>\n\n\n### 🔨 Installing\n```bash\npoetry add fastapi-cloud-drives\n```\n\n### 💻 Usage <a name="usage"></a>\nFor using fastapi-google-drive use link below\n[FastAPI Google Drive](docs/google_drive.md)\n\n\n### \nSee also the list of [contributors](https://github.com/MadeByMads/fastapi-cloud-drives/graphs/contributors) who participated in this project.\n\n\n### 👀 Contributing\nWe are open to  new ideas, additions. If you have any we would be happy to recieve and diccuss.',
     'author': 'Hasan Aliyev',
